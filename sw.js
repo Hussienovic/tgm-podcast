f9b2d615602fcfa83335c9b16863f14ea140aa86
Hussienovic <esyncedo.contact@gmail.com>
2026-09-20 06:26:15 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = "tgm-podcast-v6";
+const PRECACHE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(PRECACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== "GET" || url.origin !== location.origin) return;
+  e.respondWith(
+    caches.open(CACHE).then(async (cache) => {
+      const cached = await cache.match(e.request, { ignoreVary: true });
+      const net = fetch(e.request)
+        .then((res) => {
+          if (res && res.ok) cache.put(e.request, res.clone());
+          return res;
+        })
+        .catch(() => cached || caches.match("./index.html"));
+      return cached || net;
+    })
+  );
+});
