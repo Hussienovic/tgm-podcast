4d7215c67e6f2c6e87fc12030b9e998a55209b0a
Hussienovic <esyncedo.contact@gmail.com>
2026-09-20 08:02:17 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tgm-podcast-v10";
+const CACHE = "tgm-podcast-v11";
 const PRECACHE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
 self.addEventListener("install", (e) => {
